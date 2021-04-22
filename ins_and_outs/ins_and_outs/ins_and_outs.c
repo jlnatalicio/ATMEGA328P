b70fe4ae6ae554ecaa29910c59168cc0e2c19bb2
José Leonardo <33907953+jlnatalicio@users.noreply.github.com>
2021-04-22 19:10:55 -0300

    Update ins_and_outs.c

--- ins_and_outs/ins_and_outs/ins_and_outs.c
+++ ins_and_outs/ins_and_outs/ins_and_outs.c
@@ -2,39 +2,38 @@
  * ins_and_outs.c
  *
  * Created: 10/07/2018 00:11:11
- * Author : José Leonardo
- * Objetivo: Criar primeiro programa funcional para o Arduino UNO, para familiarização/aprendizado
- *           Nesse caso, esse programa fará um gerador de pulso de 0.5s ("Hello World da Eletrônica Digital")
+ * Author : JosÃ© Leonardo
+ * Objetivo: Criar primeiro programa funcional para o Arduino UNO, para familiarizaÃ§Ã£o/aprendizado
+ *           Nesse caso, esse programa farÃ¡ um gerador de pulso de 0.5s ("Hello World da EletrÃ´nica Digital")
  */ 
 
-//1º: definir frequência que será utilizada 
+//1Âº: definir frequÃªncia que serÃ¡ utilizada 
 #define F_CPU (16000000)				//(nesse caso, 16 MHz do cristal oscilador externo do Arduino)
 
-//2º: definir bibliotecas utilizadas
+//2Âº: definir bibliotecas utilizadas
 #include <avr/io.h>
 #include <util/delay.h>
 
-/*3º: definir funções que serão utilizadas no projeto (elas são definidas aqui, pois melhora 
-*     a visualização do código como um todo).
+/*3Âº: definir funÃ§Ãµes que serÃ£o utilizadas no projeto (elas sÃ£o definidas aqui, pois melhora 
+*     a visualizaÃ§Ã£o do cÃ³digo como um todo).
 * 
-*     Como visto no datasheet do microcontrolador, para trabalharmos com uma das GPIO, devemos utilizar a função DDRB,
+*     Como visto no datasheet do microcontrolador, para trabalharmos com uma das GPIO, devemos utilizar a funÃ§Ã£o DDRB,
 *     que funciona da seguinte forma:
 *
 * registrador = registrador OU bit_de_interesse(1) -> ligando um determinado bit
-* registrador = registrador E [NÃO bit_de_interesse(1)] -> desligando um determinado bit
+* registrador = registrador E [NÃƒO bit_de_interesse(1)] -> desligando um determinado bit
 *
-*     traduzindo para um pseudo-código: reg |= (1<<bit) e reg &= ~(1<<(bit))
+*     traduzindo para um pseudo-cÃ³digo: reg |= (1<<bit) e reg &= ~(1<<(bit))
 */
 
 #define bit_set(reg,bit) (reg |= (1<<bit))
-
 #define bit_reset(reg,bit) (reg &= ~(1<<bit))
 
 int main(void)
 {
 	bit_set (DDRB,PORTB5);	/*Como visto no datasheet, quando temos um bit do DDRx em 1, temos aquele bit do registrador como
-							 * saída. Então, usando a função que acabamos de definir, ligamos o bit 5 do DDRB (0001 0000),
-							 * definindo-o então como saída!
+							 * saÃ­da. EntÃ£o, usando a funÃ§Ã£o que acabamos de definir, ligamos o bit 5 do DDRB (0001 0000),
+							 * definindo-o entÃ£o como saÃ­da!
 							*/						
     while (1) 
     {
